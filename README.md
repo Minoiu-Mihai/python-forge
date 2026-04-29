e9bf571c474b3b1d617ef91a12b83f9a6d08dff4
Mihai Minoiu <mihaivalentin.minoiu@adatica.com>
2026-04-29 14:39:12 +0200

    feat: initial release v0.1

---
+++ README.md
@@ -0,0 +1,126 @@
+<p align="center">
+  <img src="docs/image.png" alt="python-forge banner">
+</p>
+
+<br>
+
+# Python Forge
+![Python](https://img.shields.io/badge/python-3.10%2B-blue)
+![Status](https://img.shields.io/badge/status-v0.1-green)
+![License](https://img.shields.io/badge/license-MIT-blue)
+
+> Forge your ideas. Build better Python projects.
+
+CLI tool to scaffold Python projects quickly, consistently, and professionally.
+
+## Installation
+
+Clone the repository:
+```bash
+git clone https://github.com/Minoiu-Mihai/python-forge.git
+cd python-forge
+pip install -r requirements.txt
+```
+
+## Requirements
+
+- Python 3.10+
+- pyyaml
+- Git (optional, for repository initialization)
+
+## Quick Start
+
+```bash
+python forge.py create my_project basic --python 3.12
+```
+This will:
+- Create project structure
+- Generate files
+- Setup virtual environment
+- Initialize Git repository
+
+
+
+
+## Features
+---
+- Project scaffolding from templates
+- Custom and built-in templates
+- Automatic virtual environment creation
+- Git initialization with first commit
+- Smart incremental updates (no overwrite)
+- CLI-first workflow
+
+## Templates
+
+### Built-in
+- `basic` → minimal project
+- `package` → installable Python package
+- `cli` → CLI application
+- `computer_vision` → CV/ML projects
+
+### Custom
+You can provide your own YAML template:
+
+```bash
+python forge.py create my_project path/to/template.yaml
+```
+
+
+
+
+## Usage
+
+```bash
+python forge.py create <project_name> <template> [options]
+```
+## Options
+- `--python VERSION` → select Python version
+- `--no-venv` → skip virtual environment
+- `--no-git` → skip git init
+- `--force` → apply template without confirmation
+
+
+## Example Output
+
+```bash
+INFO | Creating project: my_project.
+INFO | Creating folders...
+INFO | Creating files...
+INFO | Creating virtual environment...
+INFO | Initializing git repository...
+
+========== SUMMARY ==========
+Project: my_project
+Venv: created (Python 3.12)
+Git: initialized
+```
+
+## Philosophy
+
+Python Forge is designed to be:
+- Simple
+- Fast
+- CLI-first
+
+Not a heavy framework — just a clean, focused scaffolding tool.
+
+## Roadmap
+
+### v0.2 (next)
+- [ ] `--version` flag
+- [ ] Dry-run mode
+- [ ] Template validation
+
+### Future
+- [ ] pip install packaging
+- [ ] Remote templates (GitHub)
+- [ ] Internal modularization
+
+## Contributing
+
+- Feel free to open issues or submit PRs.
+- This project is evolving and feedback is welcome.
+
+## License
+MIT
---
+++ forge.py
@@ -0,0 +1,395 @@
+import yaml
+from pathlib import Path
+import subprocess
+import logging as l
+import argparse
+
+
+l.basicConfig(
+    level= l.INFO,
+    format= "%(levelname)s | %(message)s",
+)
+
+CONFIG_FILE = "projects.yaml"
+
+def get_default_py_version():
+
+    try:
+        version_catcher = subprocess.run(
+        ["py", "-0"],
+        check=True,
+        capture_output=True,
+        text=True, 
+        ).stdout.strip().splitlines()
+
+        versions = []
+
+        for line in version_catcher:
+            line = line.strip()
+
+            if line.startswith("-V:"):
+                version = line.split(":")[1].split()[0]
+                versions.append(version)
+        if versions:
+            versions.sort(key=lambda v: list(map(int, v.split("."))), reverse=True)
+            return versions[0]
+        else:
+            l.warning("No python version installed. Please, install python")
+            return None
+        
+    except FileNotFoundError:
+        l.error("No Python interpreter found. Cannot create virtual environment.")
+        return None
+
+    except subprocess.CalledProcessError:
+        l.error("Could not list Python versions with 'py -0'.")
+        return None
+
+def load_config(config_file):
+    file_path = Path.cwd()/config_file
+    
+    with open(file_path, "r", encoding="utf-8") as file:
+        scaffold = yaml.safe_load(file)
+
+    return scaffold
+
+
+def resolve_template(config, template_arg):
+    template_path = Path(template_arg)
+
+    if template_path.exists():
+        
+        l.info(f"Using custom template file: {template_path}")
+        
+        with open(template_path, "r", encoding="utf-8") as file:
+            custom_config = yaml.safe_load(file)
+        
+        return custom_config.get("template")
+
+    
+    if template_arg in config["templates"]:
+
+        l.info(f"Using built-in template: {template_arg}")
+
+        return config["templates"][template_arg]
+
+    l.error(f"Template '{template_arg}' not found.")
+    return None
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="python-forge",
+        description="Create Python project scaffolds quickly and consistently.",
+        epilog="Example: python forge.py create my_project basic --python 3.12",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "command",
+        choices=["create"],
+        help="Action to perform (currently only 'create')."
+    )
+
+    parser.add_argument(
+        "project_name",
+        help= "Name of the project to create."
+        )
+
+    parser.add_argument(
+        "template",
+        help="Template name from projects.yaml or path to a custom YAML template."
+    )
+    
+    default_python = get_default_py_version()
+
+    parser.add_argument(
+        "--python",
+        default=default_python,
+        metavar="VERSION",
+        help="Python version to use for the virtual environment (e.g. 3.12)." 
+    )
+
+    parser.add_argument(
+        "--no-git",
+        action="store_true",
+        help= "Skip Git initialization and first commit."
+    )
+
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        help= "Apply template to an existing project without confirmation."
+    )
+
+    parser.add_argument(
+        "--no-venv",
+        action="store_true",
+        help="Skip virtual environment creation.",
+    )
+
+
+
+
+    return parser.parse_args()
+
+
+
+
+
+def create_project(config, project_name, template_name, force = False):
+
+    template = resolve_template(config, template_name)
+    if template is not None:
+        l.info(f"Template description: {template.get('description', 'No description')}")
+        structure = template["structure"]
+        folders = structure["folders"]
+        files = structure["files"]
+    else:
+        l.error("No scaffold project detected. Please, pass a valid YAML")
+        return None
+
+    ROOT_DIR = Path(__file__).resolve().parent
+    PROJECTS_DIR = ROOT_DIR.parent
+    base_path = Path(PROJECTS_DIR/project_name)
+    l.info(f"Creating project: {project_name}.")
+    l.info(f"Target path: {base_path}")
+
+    if base_path.exists():
+        if force:
+            l.info("Project exists → forcing update.")
+        else:
+            l.warning("-------- Project already exists! ----------- \n"
+            "Do you want to update? (Y/n)")
+
+            ans = input("> ").casefold()
+
+            while ans not in ("y", "n"):
+                l.info("Please answer 'y' or 'n'")
+                ans = input("> ").casefold()
+            
+            if ans == "n":
+                l.info("Ok! Check your projects and try again.")
+                return None
+            else:
+                l.info("Project Updated!")
+    
+    else:
+        base_path.mkdir(parents = True, exist_ok=True)
+    
+    l.info("Creating Folders:")
+    for folder in folders:
+        folder = folder.replace("{{ project_name }}", project_name)
+        folder_path = Path(base_path/folder)
+        if folder_path.exists():
+            continue
+        folder_path.mkdir(parents=True, exist_ok = True)
+
+        print(f"          - {folder}")
+
+
+    l.info("Creating Files:")
+    for file, content in files.items():
+        file = file.replace("{{ project_name }}", project_name)
+        full_path = Path(base_path/file)
+
+        if full_path.exists():
+            continue
+        
+        if content is None:
+            content = ""
+
+        content = content.replace("{{ project_name }}", project_name)
+        full_path.parent.mkdir(parents=True, exist_ok=True)
+        full_path.write_text(content, encoding="utf-8")
+
+        print(f"          - {file}")
+
+    return base_path
+
+
+
+def create_venv (python_version, env_path):
+    venv_path = Path(env_path/".venv")
+    if venv_path.exists():
+        l.warning("-------- Virtual Environment already exists --------")
+        return False
+    
+    l.info("Creating virtual environment...")
+    try:
+        subprocess.run(
+            ["py", f"-{python_version}", "-m", "venv", ".venv"],
+            cwd=env_path, check= True)
+        l.info("Virtual environment created.")
+        return True
+    except subprocess.CalledProcessError as err:
+        l.error("Failed to create virtual environment.")
+        if err.stderr:
+            print(err.stderr)
+        return False
+
+
+
+def init_commit_git(project_path):
+
+    git_path = project_path/".git"
+
+    if git_path.exists():
+        l.warning("-------- Git repository already initialized --------")
+        return False
+    
+    try:
+        l.info("Check git installation")
+        check_git = subprocess.run(
+            ["git", "--version"],
+            cwd= project_path,
+            check=True, capture_output=True,
+            text=True
+        )
+        if check_git.stdout:
+            print(check_git.stdout)
+
+        l.info("Initializing git repository...")
+
+        subprocess.run(
+            ["git", "init"],
+            cwd= project_path,
+            check=True
+        )
+
+        l.info("Git repository initialized.")
+
+        try:
+            result_name = subprocess.run(
+                ["git", "config", "--global", "user.name"],
+                check=True,
+                text=True,
+                capture_output=True
+            )
+
+            result_email = subprocess.run(
+                ["git", "config", "--global", "user.email"],
+                check=True,
+                text=True,
+                capture_output=True
+            )
+            git_name = result_name.stdout.strip()
+            git_email = result_email.stdout.strip()
+
+            if not git_name or not git_email:
+                raise subprocess.CalledProcessError(1, "git config")
+
+            l.info(f"Git credentials: {git_name} -> {git_email}")
+
+            
+
+
+        except subprocess.CalledProcessError as CPE:
+            l.warning("Git credentials not configured.")
+
+            git_name = input("Enter git user.name: ").strip()
+            git_email = input("Enter git user.email: ").strip()
+            
+            subprocess.run(
+                ["git", "config", "--global", "user.name", git_name],
+                check=True
+            )
+
+            subprocess.run(
+                ["git", "config", "--global", "user.email", git_email],
+                check=True
+            )
+
+
+
+        l.info("Adding project structure to first commit...")
+        subprocess.run(
+            ["git", "add", "."],
+            cwd=project_path, check= True
+        )
+        l.info("Project structure added!")
+        l.info("Committing...")
+        git_commit_return = subprocess.run(
+            ["git", "commit", "-m", "Initial Commit"],
+            cwd=project_path, 
+            check=True, 
+            capture_output=True,
+            text=True
+        )
+        l.info("Initial commit created.")
+        if git_commit_return.stdout:
+            print(git_commit_return.stdout)
+        
+        return True
+
+    except FileNotFoundError:
+        l.error("Git is not installed or not available in PATH.")
+        
+        return False
+
+    except subprocess.CalledProcessError as err:
+        l.error("Git command failed")
+        if err.stdout:
+            print(err.stdout)
+        if err.stderr:
+            print(err.stderr)
+        
+        return False
+        
+def print_summary(project_path, args, venv_created, git_created):
+    print("\n========== SUMMARY ==========")
+
+    l.info(f"Project: {project_path.name}")
+    l.info(f"Location: {project_path}")
+
+    l.info(f"Template: {args.template}")
+
+    if args.no_venv:
+        l.info("Venv: skipped")
+    else:
+        l.info(f"Venv: {'created' if venv_created else 'Not created'} (Python {args.python})")
+
+    if args.no_git:
+        l.info("Git: skipped")
+    else:
+        l.info(f"Git: {'initialized' if git_created else 'Not initialized'}")
+    
+    print("=============================")
+
+
+
+
+def main():
+
+    args = parse_args()
+
+    scaffold = load_config(CONFIG_FILE)
+
+    project_path = create_project(scaffold,
+                                  project_name=args.project_name,
+                                  template_name=args.template,
+                                  force=args.force)
+
+    if project_path is None:
+        return
+    
+    venv_created = False
+    git_created = False
+
+    if not args.no_venv:
+        if args.python is None:
+            l.error("No Python interpreter found. Cannot create virtual environment.")
+            
+            return
+
+        venv_created = create_venv(python_version= args.python, env_path=project_path)
+    
+    if not args.no_git:
+        git_created = init_commit_git(project_path)
+    
+    print_summary(project_path, args, venv_created, git_created)
+    
+
+if __name__== "__main__":
+    main()
